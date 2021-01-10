9f141fb9487cdd8cf88188ab2a70b76e11375ae9
Bonifase Orwa <orwabonifase@gmail.com>
2021-01-10 14:07:21 +0000

    add previous and next buttons

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,4 @@
-// Change link color of the current page with CSS
+// Change link color of the current page link with CSS
 function highlightCurrent() {
     const curPage = document.URL;
     const links = document.getElementsByTagName('a');
@@ -9,14 +9,17 @@ function highlightCurrent() {
     }
 }
 
-document.onreadystatechange = () => {
-    if (document.readyState === 'complete') {
-        highlightCurrent()
-    }
-};
 
-const nextButton = document.getElementById('quize-button');
-const QUESTIONS = ["What is your gender?", "What is your profession?", "What is your Nationality?", "Where do you live?"];
+const prevButton = document.getElementById('prev-button');
+const nextButton = document.getElementById('next-button');
+
+const QUESTIONS = [
+    "What is your Gender?", 
+    "What is your Specialization?", 
+    "What is your Nationality?", 
+    "Where do you live (City)?", 
+    "What are skilled at (Primary Languange)?"
+];
 var i = 0;
 const numbers = {
     1: 'One',
@@ -27,30 +30,62 @@ const numbers = {
 }
 const choices = {
     1: ['Male', "Female", 'Prefer not to say', "Others"],
-    2: ["Doctor", "Engineer", "Teacher", "Epidemiologist"],
+    2: ["Backend Engineer", "Frontend Engineer", "Full-stack Engineer", "DevOps Engineer", "Machine Learning Engineer"],
     3: ['Kenyan', "Egyptian", "Ugandan", "Nigerian"],
     4: ['Nairobi', "Kampala", "Lagos", "Cairo", "Others"],
-    5: ['Five']
+    5: ['Python', 'Java', 'PHP', 'JavaScript', 'Typescript', 'Go', 'Ruby']
 }
 
-function getNextQuestion(){
-    let temp = i
-    document.getElementById("number").innerHTML = `Question ${numbers[temp+1]}`;
-    document.getElementById("question").innerHTML = QUESTIONS[i];
-    let questionChoices = "";
-    for(let choice of choices[temp+1]){
-        questionChoices += `<li id="chioce">${choice}</li>`
-        
-    }
-    document.getElementById("chioces").innerHTML = questionChoices;
-    console.log(temp+1, QUESTIONS[i]);
-    i++;
+// generate li element tags
+function listElement(index, choicesVals){
+    return `
+        <li>
+            <input type="radio" id="choice${index}" name=${choicesVals[index]} value=${choicesVals[index]}>
+            <label for=${choicesVals[index]}>${choicesVals[index]}</label><br>
+        </li>
+        `
 }
+
+// previous button
+function getPrevQuestion(){
+    if(i > 1){
+        i -= 1;
+        document.getElementById("number").innerHTML = `Question ${numbers[i]}`;
+        document.getElementById("question").innerHTML = QUESTIONS[i];
+        let questionChoices = "";
+        let choicesVals = choices[i];
+        for(let j=0; j < choicesVals.length; j++){
+            questionChoices += listElement(j, choicesVals);
+        }
+        document.getElementById("choices").innerHTML = questionChoices;
+    }
+}
+
+// next button
+function getNextQuestion(){
+    if(i < QUESTIONS.length){
+        let temp = i
+        document.getElementById("number").innerHTML = `Question ${numbers[temp+1]}`;
+        document.getElementById("question").innerHTML = QUESTIONS[i];
+        let questionChoices = "";
+        let choicesVals = choices[temp+1];
+        for(let j=0; j < choicesVals.length; j++){
+            questionChoices += listElement(j, choicesVals);
+            
+        }
+        document.getElementById("choices").innerHTML = questionChoices;
+        i++;
+    }
+    
+}
+
+
 document.onreadystatechange = function () {
   if (document.readyState == "complete") {
+    highlightCurrent();
     getNextQuestion();
   };
 }
 
-
+prevButton.addEventListener('click', getPrevQuestion);
 nextButton.addEventListener('click', getNextQuestion);
